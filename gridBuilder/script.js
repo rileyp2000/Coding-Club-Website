d923db66ced3746942dd0edce5ad34aaf03c7757
rileyp2000 <rileyp@bishopireton.org>
2018-11-23 20:59:51 -0500

    reorganizes stuff and working on grid creator

--- gridBuilder/script.js
+++ gridBuilder/script.js
@@ -1,10 +1,12 @@
-var row = 5;
-var col = 5;
+var row = 10;
+var col = 10;
 //var selColor = color(200,200,200);
 var squares = [];
+var reds = [];
+var yellow = [];
 
 function setup() {
-  createCanvas(301, 301);
+  createCanvas((row * 30) + 1, (col * 30) + 1);
 
   for (var r = 0; r < row; r++) {
     squares[r] = [];
@@ -12,8 +14,6 @@ function setup() {
       squares[r][c] = new Square(r, c);
     }
   }
-
-
 }
 
 function determineRow(x) {
@@ -28,7 +28,22 @@ function mousePressed() {
   var x = determineRow(mouseX);
   var y = determineCol(mouseY);
 
-  squares[x][y].selected = !squares[x][y].selected;
+  squares[x][y].selected = (squares[x][y].selected + 1) % 3;
+}
+
+function buttonSave() {
+  for (var r = 0; r < row; r++) {
+    for (var c = 0; c < col; c++) {
+      if(squares[r][c].selected != 0){
+        if(squares[r][c].selected == 1){
+          reds.push(new Array(r,c));
+        }
+        else{
+          yellow.push(new Array(r,c));
+        }
+      }
+    }
+  }
 }
 
 function draw() {
@@ -45,14 +60,20 @@ function Square(x, y) {
   this.x = x;
   this.y = y;
   this.color = color(0, 220, 88);
-  this.other = color(255, 10, 10);
-  this.selected = false;
+  this.color2 = color(255, 10, 10);
+  this.color3 = color(255,255,0);
+  this.selected = 0;
 
   this.draw = function() {
-    if (!this.selected) {
+    if (this.selected == 0) {
       fill(this.color);
     } else {
-      fill(this.other);
+      if (this.selected == 1) {
+        fill(this.color2);
+      }
+      else {
+        fill(this.color3);
+      }
     }
     rect(x * 30, y * 30, 30, 30);
   };
